b3b2dade4b419e87e596492770f10d6e2544ccb6
Mttlinh26 <linh.mtt@beetechsoft.vn>
2021-12-30 15:30:56 +0700

    demo task1 event full

--- main.js
+++ main.js
@@ -1,31 +1,57 @@
 $(document).ready(function(){
     //onclick start
-    //Cach 4 jquery
-    // $("#click1").on('click', function(){
-    //     alert('Hello4!');
-    // });
-    //Cach 5
-    // $("button").click(function(){
-    //     alert('hello5!');
-    // });
+    //Cach 5 jquery
+    $("#click5").on('click', function(){
+        alert('Hello5!');
+    });
+    //Cach 6
+    $(".click6").click(function(){
+        alert('hello6!');
+    });
     //onclick end
+
     //onchange start
     //Cach 4 jquery 
-    // $("#select2").on('change', function(){
-    //     console.log(this.value);
-    // })
-   
-    // $("#btnCheck").on('click', function() {
-    //     $("#demoChange1").change( function() {
-    //         console.log(this.value);
-    //     });
-    // });
+    $("#select-4").on('change', function(){
+        console.log(this.value);
+    });
+    $("#btn-check-4").on('click', function() {
+        $("demo-change-4").change( function() {
+            console.log(this.value);
+        });
+    });
     //Cách 5
-    // $("#select2").change(function(){
-    //     alert(this.value);
-    // })
-    $("#btnCheck").click(function() {
-        $("demoChange1").change();
-    })
+    $("#select-5").change(function(){
+        console.log(this.value);
+    });
+    $("#btn-check-4").click(function() {
+        $("#demo-change-4").change();
+    });
     //onchange end
+
+    // keyup start
+    //cach 4
+    $("#test-key-4").on('keyup', function(){
+        console.log(this.value);
+    });
+    //cach 5
+    $("#test-key-5").keyup(function() {
+        console.log(this.value);
+    });
+    //demo event.which xác định phím nào được nhấn
+    $("#test-key-6").keyup(function(event){
+        $(".event-key").html("Key is:" + event.which);
+    });
+    //keyup end
+
+    //keydown start
+    //cach 4
+    $("#key-down-4").on('keydown', function() {
+        console.log("demo4");
+    });
+    //Cách 5
+    $("#key-down-5").keydown(function(){
+        console.log("demo5");
+    });
+    //keydown end
 });
